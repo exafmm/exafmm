0bff387a391e88da1338ecff4c411af5cc05e551
Rio Yokota <rioyokota@gmail.com>
2017-03-30 16:01:47 +0300

    Cell * > Cells: 2dp

--- 2dp/fmm.cxx
+++ 2dp/fmm.cxx
@@ -35,18 +35,18 @@ int main(int argc, char ** argv) {
 
   //! Build tree
   start("Build tree");                                          // Start timer
-  Cell * cells = buildTree(bodies);                             // Build tree
+  Cells  cells = buildTree(bodies);                             // Build tree
   stop("Build tree");                                           // Stop timer
 
   //! FMM evaluation
   start("Upward pass");                                         // Start timer
-  upwardPass(cells);                                            // Upward pass for P2M, M2M
+  upwardPass(&cells[0]);                                        // Upward pass for P2M, M2M
   stop("Upward pass");                                          // Stop timer
   start("Traversal");                                           // Start timer
-  traversal(cells, cells, cycle);                               // Traversal for M2L, P2P
+  traversal(&cells[0], &cells[0], cycle);                       // Traversal for M2L, P2P
   stop("Traversal");                                            // Stop timer
   start("Downward pass");                                       // Start timer
-  downwardPass(cells);                                          // Downward pass for L2L, L2P
+  downwardPass(&cells[0]);                                      // Downward pass for L2L, L2P
   stop("Downward pass");                                        // Stop timer
 
   // Direct N-Body
