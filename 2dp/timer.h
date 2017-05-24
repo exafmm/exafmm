1ac0394530dd3a1a6a2afe004fb95d6e3ce2aba2
Tingyu Wang <vincent.tingyu.wang@gmail.com>
2017-05-24 13:48:18 -0400

    let stop timer function return time difference, used for regression test

--- 2dp/timer.h
+++ 2dp/timer.h
@@ -8,16 +8,25 @@ namespace exafmm {
   static std::map<std::string,timeval> timer;                   //!< Map of timer event name to time value
 
   //! Start timer for given event
+  /*!
+    \param event event name
+  */ 
   void start(std::string event) {
     gettimeofday(&t, NULL);                                     // Get time of day in seconds and microseconds
     timer[event] = t;                                           // Store in timer
   }
 
   //! Stop timer for given event
-  void stop(std::string event) {
+  /*!
+    \param event event name
+    \return eventTime event time in seconds
+  */
+  double stop(std::string event) {
     gettimeofday(&t, NULL);                                     // Get time of day in seconds and microseconds
-    printf("%-20s : %f s\n", event.c_str(), t.tv_sec-timer[event].tv_sec+
-           (t.tv_usec-timer[event].tv_usec)*1e-6);              // Print time difference
+    double eventTime = t.tv_sec - timer[event].tv_sec +         // Calculate event time
+                     (t.tv_usec - timer[event].tv_usec) * 1e-6;
+    printf("%-20s : %f s\n", event.c_str(), eventTime);         // Print time difference
+    return eventTime;
   }
 }
 #endif
