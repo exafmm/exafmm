a231de956d6bcf2723b95a66d229b6120c74b819
Tingyu Wang <vincent.tingyu.wang@gmail.com>
2017-05-31 18:48:41 -0400

    add getKey function for 3d 3dp in args.h

--- 3dp/args.h
+++ 3dp/args.h
@@ -19,16 +19,21 @@ namespace exafmm {
     {0, 0, 0, 0}
   };
 
+  //! Argument parser class
+  /*!
+    Parse and set the parameters of FMM and bodies from argv
+  */
   class Args {
   public:
-    int ncrit;
-    const char * distribution;
-    int numBodies;
-    int P;
-    double theta;
-    int verbose;
+    int ncrit;                                  //!< Number of bodies per leaf cell 
+    const char * distribution;                  //!< Body Distribution
+    int numBodies;                              //!< Number of bodies
+    int P;                                      //!< Order of expansions
+    double theta;                               //!< Multipole acceptance criterion
+    int verbose;                                //!< Verbose mode
 
   private:
+    //! Print the usage of option-arguments
     void usage(char * name) {
       fprintf(stderr,
               "Usage: %s [options]\n"
@@ -49,6 +54,11 @@ namespace exafmm {
               verbose);
     }
 
+    //! Parse body distribution from option-argument (optarg)
+    /*!
+      \param arg a pointer to optarg
+      \return a string of distribution name
+    */
     const char * parseDistribution(const char * arg) {
       switch (arg[0]) {
         case 'c': return "cube";
@@ -63,7 +73,30 @@ namespace exafmm {
       return "";
     }
 
+    //! Get the integer (value) mapped to body distribution (key)
+    /*!
+      \param _distribution body distribution string
+      \return the associated integer
+    */
+    uint64_t getDistNum(const char * _distribution) {
+      switch (_distribution[0]) {
+        case 'c': return 0;
+        case 'l': return 1;
+        case 'o': return 2;
+        case 'p': return 3;
+        case 's': return 4;
+        default:
+          fprintf(stderr, "invalid distribution %s\n", _distribution);
+          abort();
+      }
+      return 0;
+    }
+
   public:
+    //! Constructor
+    /*!
+      Set default values to FMM parameters and parse argv for user-defined options
+    */
     Args(int argc=0, char ** argv=NULL)
       : ncrit(64), 
         distribution("cube"),
@@ -106,6 +139,26 @@ namespace exafmm {
       }
     }
 
+    //! Calculate key based on arguments
+    /*!
+      Use bit masking to store arguments information in key, which will be
+      used in regression test to identify different runs
+      \return key
+    */
+    uint64_t getKey() {
+      uint64_t key = 0;
+      key |= uint64_t(round(log(ncrit)/log(2)));                // [1-4] bit
+      key |= getDistNum(distribution) << 4;                     // [5-15] bit
+      key |= uint64_t(round(log(numBodies)/log(10))) << 15;     // [16-19] bit
+      key |= P << 19;                                           // [20-28] bit
+      key |= uint64_t(theta*14) << 28;                          // [29-64] bit
+      return key;
+    }
+
+    //! Print formatted output for arguments
+    /*!
+      \param stringLength field width for output
+    */
     void print(int stringLength) {
       if (verbose) {
         std::cout << std::setw(stringLength) << std::fixed << std::left
