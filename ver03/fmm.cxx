1d4bb7e86dfb653e97be14ec7e54c27cc7dfd5bf
Rio Yokota <rioyokota@gmail.com>
2017-05-31 17:16:07 +0900

    Remove verify.h

--- ver03/fmm.cxx
+++ ver03/fmm.cxx
@@ -2,7 +2,6 @@
 #include "kernel.h"
 #include "timer.h"
 #include "traverse.h"
-#include "verify.h"
 using namespace exafmm;
 
 int main(int argc, char ** argv) {
@@ -11,8 +10,6 @@ int main(int argc, char ** argv) {
   ncrit = 64;                                                   // Number of bodies per leaf cell
   const int numBodies = 10000;                                  // Number of bodies
   // FMM
-  Verify verify;
-  verify.verbose = 1;                                           // Initialize a verify instance
   double totalFMM = 0;                                          // Initialize total FMM time
   printf("--- %-16s ------------\n", "FMM Profiling");          // Start profiling
   // Initialize bodies
@@ -51,13 +48,7 @@ int main(int argc, char ** argv) {
   start("Direct N-Body");                                       // Start timer
   const int numTargets = 10;                                    // Number of targets for checking answer
   Bodies jbodies = bodies;                                      // Save bodies in jbodies
-  if (numBodies > numTargets) {                                 // If bodies are more than sampled targets
-    int stride = bodies.size() / numTargets;                    //  Stride of sampling
-    for (int b=0; b<numTargets; b++) {                          //  Loop over target samples
-      bodies[b] = bodies[b*stride];                             //   Sample targets
-    }                                                           //  End loop over target samples
-    bodies.resize(numTargets);                                  //  Resize bodies
-  }                                                             // End if
+  bodies.resize(numTargets);                                    //  Resize bodies
   Bodies bodies2 = bodies;                                      // Backup bodies
   for (size_t b=0; b<bodies.size(); b++) {                      // Loop over bodies
     bodies[b].p = 0;                                            //  Clear potential
@@ -67,14 +58,13 @@ int main(int argc, char ** argv) {
   stop("Direct N-Body");                                        // Stop timer
 
   //! Verify result
-  double pDif = verify.getDifScalar(bodies, bodies2);
-  double pNrm = verify.getNrmScalar(bodies2);
+  double pDif = 0, pNrm = 0;
+  for (size_t b=0; b<bodies.size(); b++) {                      // Loop over bodies
+    pDif += (bodies[b].p - bodies2[b].p) * (bodies[b].p - bodies2[b].p);
+    pNrm += bodies2[b].p * bodies2[b].p;
+  }
   double pRel = std::sqrt(pDif/pNrm);
-  double FDif = verify.getDifVector(bodies, bodies2);
-  double FNrm = verify.getNrmVector(bodies2);
-  double FRel = std::sqrt(FDif/FNrm);
   printf("--- %-16s ------------\n", "FMM vs. direct");          //  Print message
   printf("%-20s : %8.5e s\n","Rel. L2 Error (p)", pRel);         // Print potential error
-  printf("%-20s : %8.5e s\n","Rel. L2 Error (F)", FRel);          // Print force error
   return 0;
 }
