d757de1bd432041395205333bdae567a32da1a7a
Tingyu Wang <vincent.tingyu.wang@gmail.com>
2017-08-22 10:37:02 -0400

    rename nightly -> check_all, call regression test for fmm in check_all.py

--- check_all.py
+++ check_all.py
@@ -19,7 +19,7 @@ test_params = { 'kernel': 'P',
                 'fmm': 'nPtd' }
 
 exedir_list = ['2d', '3d', '3dp', '3dp_gpu', '3dp_gpu_mpi', '3dp_simd', '3dp_simd_mpi']
-logfile = open('nightly.log', 'w')
+logfile = open('check_all.log', 'w')
 
 # loop over each directory
 for exedir in exedir_list:
@@ -35,6 +35,10 @@ for exedir in exedir_list:
             args_list = list(itertools.chain(*zip(dash_params, values)))
             # insert executable at the beginning of args list
             args_list.insert(0, os.path.join(exedir, test))
+            # make regression test for fmm executable
+            if test is "fmm":
+                args_list.insert(0, "regression.py")
+                args_list.insert(0, "python")
             # print args string and write to log file
             args_str = ' '.join(args_list)
             print(args_str)
@@ -46,5 +50,7 @@ for exedir in exedir_list:
                 logfile.write(33*'-' + '\n')
             except subprocess.CalledProcessError:
                 print("Regression Failed @", args_str)
+                print("...trying again...")
+                subprocess.call(args_list)
                 raise SystemExit
 logfile.close()
