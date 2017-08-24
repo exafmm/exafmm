48095e0fd56b5e5bf5dc3e3742aeb624aa9db394
Rio Yokota <rioyokota@gmail.com>
2017-08-24 12:18:44 +0900

    Update print.h

--- 3d/print.h
+++ 3d/print.h
@@ -10,24 +10,22 @@ namespace exafmm {
   static const int decimal = 7;                 //!< Decimal precision
 
   void print(std::string s) {
-    if (VERBOSE) {
-      s += " ";
-      std::cout << "--- " << std::setw(stringLength) << std::left
-                << std::setfill('-') << s << std::setw(decimal+1) << "-"
-                << std::setfill(' ') << std::endl;
-    }
+    if (!VERBOSE) return;
+    s += " ";
+    std::cout << "--- " << std::setw(stringLength) << std::left
+              << std::setfill('-') << s << std::setw(decimal+1) << "-"
+              << std::setfill(' ') << std::endl;
   }
 
   template<typename T>
   void print(std::string s, T v, bool fixed=true) {
-    if (VERBOSE) {
-      std::cout << std::setw(stringLength) << std::left << s << " : ";
-      if(fixed)
-        std::cout << std::setprecision(decimal) << std::fixed;
-      else
-        std::cout << std::setprecision(1) << std::scientific;
-      std::cout << v << std::endl;
-    }
+    if (!VERBOSE) return;
+    std::cout << std::setw(stringLength) << std::left << s << " : ";
+    if(fixed)
+      std::cout << std::setprecision(decimal) << std::fixed;
+    else
+      std::cout << std::setprecision(1) << std::scientific;
+    std::cout << v << std::endl;
   }
 }
 #endif
