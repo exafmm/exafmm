exafmm/exafmm

commit deb75948eed185f66ba260a47427279086e0e877

add omp task parallelism to 3dp eager horizontalPass, but now realPart in ewald has to be serial in eager mode due to thread privacy of IX, will fix this later

3dp/traverse_eager.h
@@ -2,6 +2,8 @@
 #define traverse_eager_h
 #include "exafmm.h"
 #include "kernel.h"
+#include "stdio.h"
+#include "omp.h"
 
 namespace exafmm {
   //! Recursive call to post-order tree traversal for upward pass
@@ -35,6 +37,7 @@ namespace exafmm {
       P2P(Ci, Cj);
     } else if (Cj->numChilds == 0 || (Ci->R >= Cj->R && Ci->numChilds != 0)) {
       for (Cell * ci=Ci->child; ci!=Ci->child+Ci->numChilds; ci++) {
+#pragma omp task untied if(ci->numBodies > 100) firstprivate(IX)
         horizontalPass(ci, Cj);
       }
     } else {
@@ -42,6 +45,7 @@ namespace exafmm {
         horizontalPass(Ci, cj);
       }
     }
+#pragma omp taskwait
   }
 
   //! Horizontal pass for periodic images
@@ -95,6 +99,8 @@ namespace exafmm {
 
   //! Horizontal pass interface
   void horizontalPass(Cells & icells, Cells & jcells) {
+#pragma omp parallel
+#pragma omp single
     if (IMAGES == 0) {
       horizontalPass(&icells[0], &jcells[0]);
     } else {

3dp/ewald.h
@@ -129,7 +129,9 @@ namespace exafmm {
   void realPart(Cells & icells, Cells & jcells) {
     Cells ileafs;
     getLeaf(ileafs, &icells[0]);
+#if EXAFMM_LAZY
 #pragma omp parallel for
+#endif
     for (size_t i=0; i<ileafs.size(); i++) {
       neighbor(&ileafs[i], &jcells[0]);
     }

3dp/exafmm.h
@@ -54,6 +54,8 @@ namespace exafmm {
   int IX[3];                                    //!< 3-D periodic index
   real_t CYCLE;                                 //!< Cycle of periodic boundary condition
   real_t THETA;                                 //!< Multipole acceptance criterion
+#if EXAFMM_LAZY
 #pragma omp threadprivate(IX)                   //!< Make global variables private
+#endif
 }
 #endif

commit d8bc5a7ebc151508af094160e2664f24485c81e2

remove omp parallel for in eager direct, fix bug introduced in deb7594

3dp/traverse_eager.h
@@ -145,7 +145,6 @@ namespace exafmm {
     Cj->body = &jbodies[0];
     Cj->numBodies = jbodies.size();
     int prange = (std::pow(3,IMAGES) - 1) / 2;
-#pragma omp parallel for collapse(3)
     for (int ix=-prange; ix<=prange; ix++) {
       for (int iy=-prange; iy<=prange; iy++) {
         for (int iz=-prange; iz<=prange; iz++) {