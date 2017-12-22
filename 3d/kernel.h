e0cbcf27beef402ccd6b7b2c06b0a8a226aae3bd
Rio Yokota <rioyokota@gmail.com>
2017-12-22 15:11:37 +0900

    Absorb 3d/ into 3dp/.

--- 3d/kernel.h
+++
@@ -1,9 +0,0 @@
-#if EXAFMM_LAPLACE
-#include "laplace.h"
-#elif EXAFMM_LAPLACE_KI
-#include "laplace_ki.h"
-#elif EXAFMM_HELMHOLTZ
-#include "helmholtz.h"
-#elif EXAFMM_STOKES
-#include "stokes.h"
-#endif
