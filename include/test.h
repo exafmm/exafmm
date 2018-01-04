3ac56d038974af852af63da5f8caf4056ca2287b
Tingyu Wang <vincent.tingyu.wang@gmail.com>
2018-01-04 14:29:09 -0500

    fix horizontalPass in test.h

--- include/test.h
+++ include/test.h
@@ -78,8 +78,8 @@ namespace exafmm {
     if (R2 > (Ci->R + Cj->R) * (Ci->R + Cj->R)) {
       M2L(Ci, Cj);
     } else if (Ci->numChilds == 0 && Cj->numChilds == 0) {
-      assert(Cj->numBodies != 0);
-      P2P(Ci, Cj);
+      if (Cj->numBodies == 0) M2L(Ci, Cj);
+      else P2P(Ci, Cj);
     } else if (Cj->numChilds == 0 || (Ci->R >= Cj->R && Ci->numChilds != 0)) {
       for (Cell * ci=Ci->child; ci!=Ci->child+Ci->numChilds; ci++) {
         horizontalPass(ci, Cj);
